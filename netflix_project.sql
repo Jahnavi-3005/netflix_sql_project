1e21976031fdea83f2f2870383a7e6ddab1856fc
Jahnavi-3005 <jahnavig300502@gmail.com>
2025-01-29 13:22:20 +0530

    Update netflix_project.sql

--- netflix_project.sql
+++ netflix_project.sql
@@ -17,14 +17,6 @@ create table Netflix
 
 select * from netflix
 
-select 
-     count(*) as total_content
-from netflix;
-
-select 
-     distinct type
-from netflix;
-
 -- 12 Business Problems & Solutions
 
 -- 1. Count the number of Movies vs TV Shows
